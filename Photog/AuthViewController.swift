1740e20a6536cf47a381295c1b68c2a009102dbe
Michael Gordon <mike1gordon@gmail.com>
2015-07-28 12:35:59 +0200

    Added icons to UITextFields

--- Photog/AuthViewController.swift
+++ Photog/AuthViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class AuthViewController: UIViewController {
 
+    @IBOutlet var emailTextField: UITextField?
+    @IBOutlet var passwordTextField: UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.edgesForExtendedLayout = UIRectEdge.None
+        
+        var emailImageView = UIImageView(frame: CGRectMake(0, 0, 50, self.emailTextField!.frame.size.height))
+        emailImageView.image = UIImage(named: "EmailIcon")
+        emailImageView.contentMode = .Center
+        
+        self.emailTextField!.leftView = emailImageView
+        self.emailTextField!.leftViewMode = .Always
+        
+        var passwordImageView = UIImageView(frame: CGRectMake(0, 0, 50, self.passwordTextField!.frame.size.height))
+        passwordImageView.image = UIImage(named: "PasswordIcon")
+        passwordImageView.contentMode = .Center
+        
+        self.passwordTextField!.leftView = passwordImageView
+        self.passwordTextField!.leftViewMode = .Always
     }
 
     override func viewWillAppear(animated: Bool)
@@ -21,6 +38,12 @@ class AuthViewController: UIViewController {
         super.viewWillAppear(animated)
         
         self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.emailTextField?.becomeFirstResponder()
     }
     
     override func viewWillDisappear(animated: Bool)
